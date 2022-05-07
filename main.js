e3de327cb1a94313bc48eb3a3dadb159cc32bf9a
Zhora Kentavr <zhora1kentavr@gmail.com>
2022-05-07 10:47:22 +0400

    pokazyvaet marshrut v2

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 var x0=55.790927;
 var y0=49.114453;
-var x1=55.791;
-var y1=49.115;
 var zoom0=18;
 var objMap=new Object();
 var objPlayer=new Object();
@@ -13,12 +11,33 @@ function initWindowOnload(){
 	console.log("Window onload: OK");
 }
 
+//Создание карты
 function initYMaps(){
 	console.log("YMaps init:OK.");
 	var map1=new ymaps.Map("map",{center:[x0,y0],zoom:zoom0,type: 'yandex#map'});
 	objMap=map1;
 	console.log("Center of the map1:["+ x0 + ","+y0+"]");
 	map1.events.add('click',onClickMap);
+	
+	
+	//Начальное местоположение
+	var myGeoObject4 = new ymaps.GeoObject({
+            geometry: {
+                type: "LineString",
+                coordinates: [
+                    [x0,y0],
+                    [x0,y0]
+                ]
+            },
+            properties:{
+                hintContent: "Начальное местоположение",
+            }
+        }, {
+            draggable: false,
+            strokeColor: "#0000FF",
+            strokeWidth: 20
+        });
+		map1.geoObjects.add(myGeoObject4)
 	
 	
 	//Панорама
@@ -49,14 +68,14 @@ function onClickMap(e){
 	console.log("Clicked map:coordsClick = [" + coordsClick + "]");
 	var placemarkClick = new ymaps.Placemark(coordsClick,{balloonContentHeader: 'User Clicked',balloonContentBody:'LON:'+coordsClick[1]+'<br> LAT : ' + coordsClick[0],balloonContentFooter: 'Kazan City',hintContent:'LON:' +coordsClick[1]+'<br> LAT: '+coordsClick[0]});
 	objMap.geoObjects.add(placemarkClick);
-	placemarkClick.balloon.open();
-	player.moveTo(coordsClick);
 }
 
+//При перемещение по панораме
 function onPanoramaChange(){
 	var panorama=objPlayer.getPanorama();
 	var newPos=panorama.getPosition();
 	console.log(newPos);
+	
 	var map1=objMap;
 		  var myGeoObject = new ymaps.GeoObject({
             geometry: {
@@ -71,13 +90,50 @@ function onPanoramaChange(){
             }
         }, {
             draggable: false,
-            strokeColor: "#0000FF",
+            strokeColor: "#000000",
             strokeWidth: 10
+        });
+		
+		var myGeoObject2 = new ymaps.GeoObject({
+            geometry: {
+                type: "LineString",
+                coordinates: [
+                    [x0, y0],
+                    [newPos[0],newPos[1]]
+                ]
+            },
+            properties:{
+                hintContent: "Маршрут",
+            }
+        }, {
+            draggable: false,
+            strokeColor: "#FFFFFF",
+            strokeWidth: 5
+        });
+				var myGeoObject3 = new ymaps.GeoObject({
+            geometry: {
+                type: "LineString",
+                coordinates: [
+                    [newPos[0],newPos[1]],
+                    [newPos[0],newPos[1]]
+                ]
+            },
+            properties:{
+                hintContent: "Текущее местоположение",
+            }
+        }, {
+            draggable: false,
+            strokeColor: "#FF0000",
+            strokeWidth: 20
         });
 
 
     // Добавляем линии на карту.
-    map1.geoObjects.add(myGeoObject);
+	
+    map1.geoObjects.add(myGeoObject)
+				   .add(myGeoObject2)
+				   .add(myGeoObject3)
+				   .splice(3,1,myGeoObject3);
 	x0=newPos[0];
 	y0=newPos[1];
 }
