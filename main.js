15a3f1480da00dbbb6b919765d9f6fc93ddd8a37
ZhoraKentavr <zhora1kentavr@gmail.com>
2022-05-12 15:08:20 +0400

    dobavil izmenenie polozheniya panoramy pri nazhatii na kartu,knopki dlya obnovleniya marshruta,vozvrata na predyduschie shagi marshruta, knopki dlya peremescheniya v druge goroda

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
+var x=x0;
+var y=y0;
 var x0=55.790927;
 var y0=49.114453;
 var zoom0=18;
 var objMap=new Object();
 var objPlayer=new Object();
+var objArray=new Object();
+var cnt=0;
 
 window.onload=initWindowOnload;
 ymaps.ready(initYMaps);
 
 function initWindowOnload(){
 	console.log("Window onload: OK");
+	coordinates.textContent="Текущие координаты : [" + x0 + "],[" + y0 + "]";
+	let coordArray = new Array();
+	objArray=coordArray;
 }
 
 //Создание карты
@@ -46,15 +53,11 @@ function initYMaps(){
 	}
 	ymaps.panorama.locate([x0,y0]).done(
 	function(panoramas){
-		console.log("panoramas:"+panoramas);
-		console.log("panoramas[0]:"+panoramas[0]);
-		console.log("panoramas length :"+ panoramas.length);
-		console.log("panoramas coord :"+ panoramas);
 		if(panoramas.length>0){
 			var player=new ymaps.panorama.Player(
 				'player',panoramas[0],{direction:[140,15]}
 				);
-		}
+		} 
 		objPlayer=player;
 		player.events.add('panoramachange', onPanoramaChange);
 
@@ -65,19 +68,28 @@ function initYMaps(){
 
 function onClickMap(e){
 	var coordsClick=e.get('coords');
+	console.log('coords');
 	console.log("Clicked map:coordsClick = [" + coordsClick + "]");
-	var placemarkClick = new ymaps.Placemark(coordsClick,{balloonContentHeader: 'User Clicked',balloonContentBody:'LON:'+coordsClick[1]+'<br> LAT : ' + coordsClick[0],balloonContentFooter: 'Kazan City',hintContent:'LON:' +coordsClick[1]+'<br> LAT: '+coordsClick[0]});
-	objMap.geoObjects.add(placemarkClick);
+	player=objPlayer;
+	player.moveTo([coordsClick[0],coordsClick[1]]);
 }
+
 
 //При перемещение по панораме
 function onPanoramaChange(){
 	var panorama=objPlayer.getPanorama();
 	var newPos=panorama.getPosition();
-	console.log(newPos);
-	
+	var x1=newPos[0];
+	var y1=newPos[1];
 	var map1=objMap;
-		  var myGeoObject = new ymaps.GeoObject({
+	
+	coordArray=objArray;
+	coordArray.push([x1,y1]);
+	console.log("перемещение");
+	console.log(coordArray);
+	
+	//1
+		 var myGeoObject = new ymaps.GeoObject({
             geometry: {
                 type: "LineString",
                 coordinates: [
@@ -91,9 +103,10 @@ function onPanoramaChange(){
         }, {
             draggable: false,
             strokeColor: "#000000",
-            strokeWidth: 10
+            strokeWidth: 10,
         });
 		
+	//2
 		var myGeoObject2 = new ymaps.GeoObject({
             geometry: {
                 type: "LineString",
@@ -109,8 +122,10 @@ function onPanoramaChange(){
             draggable: false,
             strokeColor: "#FFFFFF",
             strokeWidth: 5
-        });
-				var myGeoObject3 = new ymaps.GeoObject({
+        }); 
+		
+	//3
+	    var myGeoObject3 = new ymaps.GeoObject({
             geometry: {
                 type: "LineString",
                 coordinates: [
@@ -129,11 +144,93 @@ function onPanoramaChange(){
 
 
     // Добавляем линии на карту.
-	
     map1.geoObjects.add(myGeoObject)
 				   .add(myGeoObject2)
 				   .add(myGeoObject3)
 				   .splice(3,1,myGeoObject3);
+				   
 	x0=newPos[0];
 	y0=newPos[1];
+	
+	//Счётчик маршрута (Сделал условие, что текущие координаты Больше ИЛИ Меньше предыдущих)
+	if (newPos[0]>newPos[1])
+	{
+		cnt++;
+		console.log("Шагов панорамы сделано: "+cnt);
+	} else if (newPos[0]<newPos[1])
+	{
+		cnt++;
+		console.log("Шагов панорамы сделано: "+cnt);
+	}
+	counter.textContent="Счётчик шагов : "+cnt;
+	coordinates.textContent="Текущие координаты : [" + newPos[0] + "],[" + newPos[1] + "]";
+}
+
+//При нажатии на кнопку "Обновить"
+function onClickUpdate(){
+	//cnt - Счётчик, обнуляется при обновлении маршрута
+	cnt=0;
+	var map1=objMap;
+	map1.geoObjects.removeAll();
+	
+		var myGeoObject4 = new ymaps.GeoObject({
+            geometry: {
+                type: "LineString",
+                coordinates: [
+                    [x0,y0],
+                    [x0,y0]
+                ]
+            },
+            properties:{
+                hintContent: "Начальное местоположение",
+            }
+        }, {
+            draggable: false,
+            strokeColor: "#0000FF",
+            strokeWidth: 20
+        });
+		map1.geoObjects.add(myGeoObject4)
+		
+		counter.textContent="Счётчик шагов : 0";
+		
+		coordArray=objArray;
+		for(var i=coordArray.length;i>0;i--){
+			coordArray.pop();}
+
+}
+function onClickBack(){
+	coordArray=objArray;
+	player=objPlayer;
+	map=objMap;
+	player.moveTo(coordArray[coordArray.length-2]);
+	if(coordArray.length>1){
+		coordArray.splice(coordArray.length-2,2);
+		}
+		else{
+			player.moveTo([x,y]);
+		}
+	cnt-=2;
+	console.log("кнопка");
+	console.log(coordArray);
+}
+//Для гиперссылок на города
+function kyrov(){
+player=objPlayer;
+map=objMap;
+player.moveTo([58.602685, 49.630212]);
+map.setCenter([58.602685, 49.630212]);
+}
+
+function Perm(){
+player=objPlayer;
+map=objMap;
+player.moveTo([58.009554, 56.226651]);
+map.setCenter([58.009554, 56.226651]);
+}
+
+function Izhevsk(){
+player=objPlayer;
+map=objMap;
+player.moveTo([56.850195, 53.203126]);
+map.setCenter([56.850195, 53.203126]);
 }
